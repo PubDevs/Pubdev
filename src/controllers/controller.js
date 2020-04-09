08d252b032a9af67682429bba8d193edd214b941
MXR192 <Andrres017@gmail.com>
2020-04-08 20:33:01 -0500

    Actualizado a mvc
    la expliacion se los dejo en telegram

---
+++ src/controllers/controller.js
@@ -0,0 +1,16 @@
+
+exports.renderIndexPage = (req, res) => {
+    res.render("index")
+}
+exports.renderEventosPage = (req, res) => {
+    res.render("eventos")
+}
+exports.renderRegistroPage = (req, res) => {
+    res.render("registro")
+}
+exports.renderEventoPage = (req, res) => {
+    res.render("generic")
+}
+exports.renderSalondefamaPage = (req, res) => {
+    res.render("salonFama")
+}
---
+++ src/router.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("./controllers/controller")
+
+router.get("/", controller.renderIndexPage)
+router.get("/eventos", controller.renderEventosPage)
+router.get("/registro", controller.renderRegistroPage)
+router.get("/evento", controller.renderEventoPage)
+router.get("/salondefama", controller.renderSalondefamaPage)
+
+module.exports = router
