3b863722adb2448645d74cb66a6d81997490dce5
MXR192 <Andrres017@gmail.com>
2020-05-12 22:15:24 -0500

    Update controllerRegistro.js

--- src/controllers/controllerRegistro.js
+++ src/controllers/controllerRegistro.js
@@ -11,7 +11,7 @@ controllerRegistro.renderRegistroPage = (req, res) => {
     res.render("../../views/registro")
 }
 controllerRegistro.registrarUsuario = async (req, res) => {
-	if(validarInformacion(req.body.nombre,req.body.correo,req.body.sobreNombre,req.body.contrasena1,req.body.contrasena2,req.body.BackEnd,req.body.FrontEnd)){
+	if(validarInformacion((req.file).originalname,req.body.nombre,req.body.correo,req.body.sobreNombre,req.body.contrasena1,req.body.contrasena2,req.body.BackEnd,req.body.FrontEnd)){
 		const newModeloRegistro = new ModelRegistro(req.body,this.db)
 		var datos = await newModeloRegistro.crearUsuarionew()
 		if(datos){
@@ -26,9 +26,21 @@ controllerRegistro.registrarUsuario = async (req, res) => {
 }
 
 
-function validarInformacion(nombre, correo, sobreNombre, contrasena1, contrasena2, BackEnd, FrontEnd){
-	return (nombre !== "" && validarEmail(correo) && sobreNombre !== "" &&  validarContrasena(contrasena1, contrasena2) && BackEnd !== "Back-end" && FrontEnd !== "Front-End") ? true : false;
+function validarInformacion(originalname,nombre, correo, sobreNombre, contrasena1, contrasena2, BackEnd, FrontEnd){
+	return (validarImg(originalname) && nombre !== "" && validarEmail(correo) && sobreNombre !== "" &&  validarContrasena(contrasena1, contrasena2) && BackEnd !== "Back-end" && FrontEnd !== "Front-End") ? true : false;
 	
+}
+function validarImg(nombre){
+	var nombre = nombre.toLowerCase();
+	/*for(var i=0; i<nombre.length;i++){
+		console.log()
+	}*/
+	var allowedExtensions = /(.jpg|.jpeg|.png)$/i;
+    if(allowedExtensions.exec(nombre)){
+		return true
+	}else{
+		return false
+	}
 }
 
 function validarContrasena(pass1, pass2){
